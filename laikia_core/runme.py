aa9b1a91da93ae6787c2d8c188ecff38b530a82c
James Dressel <ubergeek@jamesdressel.com>
2013-03-09 18:47:29 -0500

    Create loop to add players

--- laikia_core/runme.py
+++ laikia_core/runme.py
@@ -3,10 +3,12 @@ from event import event
 from character import character
 from item import item
 
+
 def main():
     #Build ship
     laika = ship("Laika", 100.0, 100.0, 100.0)
-    
+    characters = []
+        
     event_solar_flare = event("Solar Flare", "Solar Flare hit, damaging com system", -10, 0, 0)
     event_oxygen_fire = event("Fire in oxygen garden", "Oh god, fire in the oxygen garden.", 0, -3.0, -20.9)
     
@@ -23,7 +25,17 @@ def main():
     
     
     # Loop to create characters
+    num_players_str = raw_input("How many players: ")
+    num_players = int(num_players_str)
+
+
     
+    for _ in xrange(num_players):
+        #TODO add choice of character abilities
+        name = raw_input()
+        character_a = character(name, 100.0, 2.0, 1.0, .1) #100 health 2 water, 1 oxygen, .1 com
+        characters.append(character_a)
+
     # Setup Loop
     
     gun = item("gun", "When used, a gun prevents the owner from being voted into the reactor")
