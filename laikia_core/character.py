d65d73515a9476dae9709f26737277f2697bd469
James Dressel <ubergeek@jamesdressel.com>
2013-03-17 16:02:06 -0400

    Add health and reactor

--- laikia_core/character.py
+++ laikia_core/character.py
@@ -1,3 +1,4 @@
+import random
 class character:
     def __init__(self, name, health, water_skill, oxygen_skill, com_skill):
         self.name = name
@@ -15,4 +16,14 @@ class character:
             "%\n\t Water Skill: " + str(self.water_skill) + \
             "\n\t Oxygen Skill: " + str(self.oxygen_skill) + \
             "\n\t Com Systems Skill: " + str(self.com_skill)
-            
+    def into_the_reactor(self):
+        print "into the reactor"
+        #todo factor in individual radiation resistance and items
+        self.health = self.health - random.uniform(30, 50)
+        self.health = -3
+        
+    def alive(self):
+        if (self.healh > 0):
+            return True
+        else:
+            return False
